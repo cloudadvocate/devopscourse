8a504163a3f06846a5b82a21809185a3a4430f22
cloudadvocate <multicloudguy@gmail.com>
2023-05-27 12:10:22 +0530

    addedd comment

--- module6/docker-multistage/src/component/Display.js
+++ module6/docker-multistage/src/component/Display.js
@@ -11,6 +11,7 @@ export default class Display extends React.Component {
   render() {
     return (
       <div className="component-display">
+        <h3>Welcome to DevOps Course</h3>
         <div>{this.props.value}</div>
       </div>
     );
