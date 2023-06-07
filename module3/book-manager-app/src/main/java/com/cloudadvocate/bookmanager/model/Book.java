ea3b9e50e7ce2ec565b170e36c1f7ad30ac8582f
cloudadvocate <multicloudguy@gmail.com>
2023-06-07 07:58:11 +0530

    added ui for book

--- module3/book-manager-app/src/main/java/com/cloudadvocate/bookmanager/model/Book.java
+++ module3/book-manager-app/src/main/java/com/cloudadvocate/bookmanager/model/Book.java
@@ -1,72 +1,75 @@
 package com.cloudadvocate.bookmanager.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "book")
 public class Book {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-  @Column(name = "title")
-  private String title;
+	@Column(name = "title")
+	@NotBlank(message = "Title is mandatory")
+	private String title;
 
-  @Column(name = "description")
-  private String description;
+	@Column(name = "description")
+	@NotBlank(message = "Description is mandatory")
+	private String description;
 
-  @Column(name = "published")
-  private boolean published;
+	@Column(name = "published")
+	private boolean published = false;
 
-  public Book() {
+	public Book() {
 
-  }
+	}
 
-  public Book(String title, String description, boolean published) {
-    this.title = title;
-    this.description = description;
-    this.published = published;
-  }
-  
-  public Book(long id, String title, String description, boolean published) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.published = published;
-  }
+	public Book(String title, String description, boolean published) {
+		this.title = title;
+		this.description = description;
+		this.published = published;
+	}
 
-  public long getId() {
-    return id;
-  }
+	public Book(long id, String title, String description, boolean published) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.published = published;
+	}
 
-  public String getTitle() {
-    return title;
-  }
+	public long getId() {
+		return id;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public String getTitle() {
+		return title;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public String getDescription() {
+		return description;
+	}
 
-  public boolean isPublished() {
-    return published;
-  }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-  public void setPublished(boolean isPublished) {
-    this.published = isPublished;
-  }
+	public boolean isPublished() {
+		return published;
+	}
 
-  @Override
-  public String toString() {
-    return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
-  }
+	public void setPublished(boolean isPublished) {
+		this.published = isPublished;
+	}
+
+	@Override
+	public String toString() {
+		return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+	}
 
 }
---
+++ module3/book-manager-app/src/main/java/com/cloudadvocate/bookmanager/ui/BookManagerUIController.java
@@ -0,0 +1,24 @@
+package com.cloudadvocate.bookmanager.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cloudadvocate.bookmanager.controller.BookManagerController;
+
+//https://www.geeksforgeeks.org/spring-boot-thymeleaf-with-example/
+
+@Controller
+public class BookManagerUIController {
+
+	@Autowired
+	private BookManagerController bookManagerController;
+
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("allbooks", this.bookManagerController.getAllbooks(null));
+		return "index";
+	}
+
+}
