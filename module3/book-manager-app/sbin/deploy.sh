981a9d7df8b685d84a4493ce18a7af8c8177d5df
cloudadvocate <multicloudguy@gmail.com>
2023-06-10 14:50:50 +0530

    updated service port

--- module3/book-manager-app/sbin/deploy.sh
+++ module3/book-manager-app/sbin/deploy.sh
@@ -2,7 +2,7 @@
 
 VERSION=1.0.0-SNAPSHOT
 ARTIFACT_NAME=$(aws codeartifact list-package-version-assets --domain cloudadvocate --domain-owner 363267848264 --repository book-manager-repository --format maven --namespace com.cloudadvocate.java --package bookmanager --package-version 1.0.0-SNAPSHOT --query 'assets[0].name' | sed 's/\"//g')
-SERVICE_PORT=8080
+SERVICE_PORT=8081
 
 if [ ! -f  "dd-java-agent.jar" ] 
 then
